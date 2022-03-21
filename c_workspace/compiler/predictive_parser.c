b3a82c7b8d0e97b3c1abe31d474d8d163399da81
TaeHyun Park <pthkuku99@gmail.com>
2022-03-21 22:04:35 +0900

    add match, nexttoken fucntions

---
+++ c_workspace/compiler/predictive_parser.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+char lookahead;
+
+void main(){
+        if(lookahead == '$')
+            printf("\n");
+        else
+            printf(" error\n");
+}
+
+void match(char token) {
+    if(lookahead == token)
+        lookahead = nexttoken();
+    else {
+            printf("error\n");
+            exit(1);
+    }
+}
+
+char nexttoken() {
+    char c;
+
+    while(1){
+            c = getchar();
+            if(c==' '|| c=='\t'|| c == '\n' || c == '\0') continue;
+            return(c);
+    }
+}
+
