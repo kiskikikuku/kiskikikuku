169b8bdc05106e8578c3db4973b96f286e355d86
TaeHyun Park <pthkuku99@gmail.com>
2022-03-26 09:18:37 +0900

    "First Commit"

--- c_workspace/compiler/predictive_parser.c
+++ c_workspace/compiler/predictive_parser.c
@@ -1,17 +1,22 @@
 #include<stdio.h>
-
+char nexttoken();
+void expr();
+void rest1();
+void term();
+void rest2();
+void factor();
 char lookahead;
 
 void main(){
         if(lookahead == '$')
-            printf("\n");
+            printf("parsing complete successfully\n");
         else
             printf(" error\n");
 }
 
 void match(char token) {
     if(lookahead == token)
-        lookahead = nexttoken();
+        lookahead = nexttoken(); //예측기호와 같으면 다음 토큰받음
     else {
             printf("error\n");
             exit(1);
@@ -29,23 +34,44 @@ char nexttoken() {
 }
 
 void expr(){
-    if(lookahead == ()    
+    term(); printf("3 "); rest1();
+
+    return;
 }
 
 void rest1(){
+    if (lookahead == '+'){
+        match('+'); term(); print("1 "); rest1();
+    }
+    else if(lookahead = '-')
+        match('-'); term(); print("2 "); rest1();
 
+
+    
 }
 
 void term(){
-
+    factor(); printf("6 "); rest2();
 }
 
 void rest2(){
-
+    if (lookahead == '*'){
+        match('*'); term(); print("4 "); rest2();
+    }
+    else if(lookahead = '-')
+        match('-'); term(); print("5 "); rest2();
+    
 }
 
 void factor(){
-    
+    if (lookahead == '('){
+        match('('); expr(); match(')'); printf("7 ");
+    }
+    else if (isdigit(lookahead)){
+        match(lookahead);
+    }
+    else 
+        printf("error");
 }
 
 
